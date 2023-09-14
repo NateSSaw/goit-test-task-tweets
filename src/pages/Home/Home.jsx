d340a9e21d554e95b24373e61d4bead4402ea09d
Nataliia Semenova <natasemenova1806@gmail.com>
2023-09-14 21:43:04 +0300

    done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Welcome. Nice to meet you here.) You can see here a lot of tweets</h1>
+      <h1>Welcome. Nice to meet you here.) You can see here a lot of tweets and followed</h1>
     </div>
   );
 }
